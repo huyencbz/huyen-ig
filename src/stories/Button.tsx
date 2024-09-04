8ebef85190e3781279e1536a4a152b7925977f5f
h001683 <huyen-bui@cybozu.vn>
2024-09-04 18:00:24 +0700

    Create Button component

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,52 +1,14 @@
-import React from "react";
-import "./button.css";
-
+import React, { ReactNode } from "react";
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  children?: ReactNode;
 }
-
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+export const Button = ({ children }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " ",
-      )}
-      style={{ backgroundColor }}
-      {...props}
+      className="bg-blue-500 hover:bg-blue-700 text-white text-sm py-2 px-40 border-blue-700 rounded"
     >
-      {label}
+      {children}
     </button>
   );
 };
