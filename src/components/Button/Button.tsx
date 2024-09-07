9b93897e412241e7e7a0509f82cc992e5a0c8ca0
h001683 <huyen-bui@cybozu.vn>
2024-09-07 18:31:12 +0700

    Update padding button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export const Button = ({ children }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="bg-blue-500 hover:bg-blue-700 text-white text-sm py-2 px-40 border-blue-700 rounded"
+      className="bg-blue-500 hover:bg-blue-500 text-white text-sm py-2 px-32 border-blue-700 rounded"
     >
       {children}
     </button>
