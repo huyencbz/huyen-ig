04a31c4be7653ca0ea84e37eaf0649ba1263db80
h001683 <huyen-bui@cybozu.vn>
2024-09-13 17:35:59 +0700

    Add Divider component

---
+++ src/components/Divider/Divider.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Divider } from "./Divider";
+
+const meta: Meta<typeof Divider> = {
+  component: Divider,
+};
+
+export default meta;
+type Story = StoryObj<typeof Divider>;
+
+export const Normal: Story = {
+  args: {},
+};
---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Divider: FC = () => {
+  return <div className="border border-gray-100"></div>;
+};
