491b33853c19c6b030d6c7032723fdf3f4e0cff8
h001683 <huyen-bui@cybozu.vn>
2024-09-13 17:36:32 +0700

    Update code index screen

---
+++ src/routes/index.tsx
@@ -0,0 +1,37 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+import logo from "../assets/logo.svg";
+import { Avatar } from "../components/Avatar/Avatar";
+import { Button } from "../components/Button/Button";
+import { Divider } from "../components/Divider/Divider";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <>
+      <div className="flex items-center flex-col h-screen justify-center">
+        <div className="flex flex-col items-center justify-center w-2/3">
+          <img className="mb-10" src={logo}></img>
+          <div className=" flex flex-col gap-2 mb-2 items-center">
+            <Avatar size="large" />
+            <span className="text-sm">huyen_cute</span>
+          </div>
+          <Button isBlock>Login</Button>
+          <Link className="mt-6 text-sm" to="/">
+            Switch accounts
+          </Link>
+        </div>
+      </div>
+      <div className="relative">
+        <div className="absolute inset-x-0 bottom-0 h-24">
+          <Divider />
+          <div className="text-sm mt-4 text-center text-gray-400">
+            Don't have an account? <Link to="/">Sign up</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
