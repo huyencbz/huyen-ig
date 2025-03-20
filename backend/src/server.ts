7f74e831be83b41549461f82558612c19a77e5c8
h001683 <huyen-bui@cybozu.vn>
2025-03-20 16:55:21 +0700

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
-import accountRouters from "./src/accounts/routes";
+import accountRouters from "./accounts/routes";
 
 const app = express();
 app.use(
